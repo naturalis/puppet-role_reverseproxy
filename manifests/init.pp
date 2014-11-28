f92e371e6ac816a31a6a5e10f709cd255e3a559f
Atze de Vries <atze.devries@naturalis.nl>
2014-11-28 12:51:04 +0100

    added new site

--- manifests/init.pp
+++ manifests/init.pp
@@ -66,7 +66,7 @@ class role_reverseproxy {
   nginx::resource::vhost {'www.catalogueoflife.* catalogueoflife.*': proxy    => 'http://134_213_57_40', }
 
   nginx::resource::location{ '46_149_29_25_testcol':
-    location => '~^/testcol*$',
+    location => '/testcol',
     vhost    => 'www.catalogueoflife.* catalogueoflife.*',
     proxy    => 'http://46_149_29_25/$realpath_root'
   }
